58e8e4fd04ed0bf0049bd93facf5b75f850b8d26
Luke Edwards <luke.edwards05@gmail.com>
2016-12-13 11:13:43 -0800

    configure linter

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
-import buble from 'rollup-plugin-buble'
-import replace from 'rollup-plugin-replace'
-import resolve from 'rollup-plugin-node-resolve'
+import buble from 'rollup-plugin-buble';
+import replace from 'rollup-plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
 	dest: 'dist/bundle.js',
@@ -8,11 +8,13 @@ export default {
 	format: 'iife',
 	plugins: [
 		buble(),
-		replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+		replace({
+			'process.env.NODE_ENV': JSON.stringify('development')
+		}),
 		resolve({
 			browser: true,
 			main: true
 		})
 	],
 	sourceMap: true
-}
+};
