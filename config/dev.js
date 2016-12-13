6eada19fd722669c769bbbb7e201a9981687aa96
Luke Edwards <luke.edwards05@gmail.com>
2016-12-13 11:12:42 -0800

    install rollup & configure

---
+++ config/dev.js
@@ -0,0 +1,18 @@
+import buble from 'rollup-plugin-buble'
+import replace from 'rollup-plugin-replace'
+import resolve from 'rollup-plugin-node-resolve'
+
+export default {
+	dest: 'dist/bundle.js',
+	entry: 'src/index.js',
+	format: 'iife',
+	plugins: [
+		buble(),
+		replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+		resolve({
+			browser: true,
+			main: true
+		})
+	],
+	sourceMap: true
+}
